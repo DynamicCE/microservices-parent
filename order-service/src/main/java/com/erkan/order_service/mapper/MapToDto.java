2ce5d3d44bb1fa2bdd1db049a36747a2002b246e
Adınız <erkanmurat0632@gmail.com>
2024-11-10 19:21:30 +0300

    fix : @Component anotasyonu eklendi

--- order-service/src/main/java/com/erkan/order_service/mapper/MapToDto.java
+++ order-service/src/main/java/com/erkan/order_service/mapper/MapToDto.java
@@ -2,10 +2,12 @@ package com.erkan.order_service.mapper;
 
 import com.erkan.order_service.dto.OrderLineItemsDto;
 import com.erkan.order_service.model.OrderLineItems;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MapToDto {
 
-public OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+    public OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
         OrderLineItems orderLineItems = new OrderLineItems();
         orderLineItems.setPrice(orderLineItemsDto.getPrice());
         orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
